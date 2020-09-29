31ccab11ebd95a5b1d9837602287949bcf6a95f2
Michael Zachor <mfz226@nyu.edu>
2020-09-29 15:33:17 -0400

    Basic components and data placeholders

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
+const weatherKey = `c60878fca060ccd07165aaf1f13d5ee8`;
+
 function Home() {
+// Displays:
+// Weather Type (ex. Cloudy)
+// Current Temperature
+// High Temperature
+// Low Temperature
+// Cloudiness
+// Humidity
+// Wind Speed
+
     return (
-        <div>
-            <h1>Weather App</h1>
-        </div>
+        <>
+            <Header />
+            <main className="Home">
+                <h3>Weather in Seoul</h3>
+                <div className="WeatherInfo">
+                    <p>Weather Type: Cloudy</p>
+                    <p>Current Temperature: 100 Degrees</p>
+                    <p>High Temperature: 100 Degrees</p>
+                    <p>Low Temperature: 80 Degrees </p>
+                    <p>Cloudiness: 100</p>
+                    <p>Humidity: 35%</p>
+                    <p>Wind Speed: 3 km/h</p>
+
+                </div>
+            </main>
+        </>
         
-    )
+    );
 }
 
 export default Home;
